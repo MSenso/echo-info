09e8034b169fdf25c5acf72c6a9218bca99f94b7
ChDarina <dvchupina@edu.hse.ru>
2023-03-19 00:57:53 +0500

    fixed add-rows.js

--- frontend/html/resources/js/add-rows.js
+++ frontend/html/resources/js/add-rows.js
@@ -1,17 +1,19 @@
-// Get the table and input values
-var table = document.getElementById("myTable");
-var id = document.getElementById("id").value;
-var items = document.getElementById("items").value.split(",");
+function addRow() {
+    // Get the table and input values
+    var table = document.getElementById("myTable");
+    var id = document.getElementById("id").value;
+    var items = document.getElementById("items").value.split(",");
 
-// Create a new row and cells
-var newRow = table.insertRow(-1);
-var idCell = newRow.insertCell(0);
-var itemsCell = newRow.insertCell(1);
+    // Create a new row and cells
+    var newRow = table.insertRow(-1);
+    var idCell = newRow.insertCell(0);
+    var itemsCell = newRow.insertCell(1);
 
-// Set the cell values
-idCell.innerHTML = id;
-itemsCell.innerHTML = "[" + items.join(", ") + "]";
+    // Set the cell values
+    idCell.innerHTML = id;
+    itemsCell.innerHTML = "[" + items.join(", ") + "]";
 
-// Clear the input values
-document.getElementById("id").value = "";
-document.getElementById("items").value = "";
+    // Clear the input values
+    document.getElementById("id").value = "";
+    document.getElementById("items").value = "";
+}
